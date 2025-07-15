b1662dbdb0c06f84f96d9fe6111d31f40ee98c3d
Ivano <ivano.barletta@nowsystems.eu>
2025-07-15 11:16:59 +0200

    abs path of nemo/xios, date of prev cycle

--- create_suite.py
+++ create_suite.py
@@ -5,9 +5,9 @@ from os.path import join
 
 nMembers        = 1         # 4
 hindcastYear1   = 1993      # 1993
-hindcastYear2   = 1994      # 2019
+hindcastYear2   = 1993      # 2019
 
-hindcast_month_periods = 4 # 62
+hindcast_month_periods = 1 # 62
 
 model_dt = pd.Timedelta(seconds = 900)  # 900 sec
 
@@ -32,16 +32,19 @@ suite.add_variable("LOG_BASE_DIR"       , f"{main_dir}/logs")
 suite.add_variable("NPROC_NEMO"         , 100)
 suite.add_variable("NPROC_XIOS"         , 10)
 
-suite.add_variable("HOST"           , 'login210-19')
-suite.add_variable("USER"           , 'emnowiba')
-suite.add_variable("ECF_TRYNO"      , '1')
+suite.add_variable("HOST"           , "login210-19")
+suite.add_variable("USER"           , "emnowiba")
+suite.add_variable("ECF_TRYNO"      , "1")
 suite.add_variable("MEMBERS"        ,nMembers)              # 4
 suite.add_variable("CYCLES"         ,hindcast_month_periods) # 62
 
 suite.add_variable("HINDCAST_YEAR1", hindcastYear1) # 1993
 suite.add_variable("HINDCAST_YEAR2", hindcastYear2) # 2019
 
-suite.add_variable("run_time"       ,'00:01:00')
+suite.add_variable("run_time"       ,"00:01:00")
+
+suite.add_variable("NEMOEXE"        ,"")    # absolute path of NEMO executable
+suite.add_variable("XIOSEXE"        ,"")    # absolute path of XIOS executable
 
 membersList = [str(mem).zfill(3) for mem in range(nMembers)]    # ['000','001','002',...]
 
@@ -68,6 +71,7 @@ for member in membersList:
         hindcast_family.add_variable("HINDCAST_END_DATE"    , f"{hindcast_end_year}1231")      # 31st Dec
         hindcast_family.add_variable("HINDCAST_PERIOD"      , f"{hindcast_start_year}-{hindcast_end_year}")
 
+
         hindcast_start_date = "%d1101" % hindcast_start_year    # 1101 of each year
 
         # dates of monthly sub-simulations (or cycles) 
@@ -83,7 +87,13 @@ for member in membersList:
             cycle_duration = hindcast_periods[icycle+1] - hindcast_periods[icycle]
 
             cycle_start_date        = hindcast_periods[icycle]
+            if icycle > 0:     
+                cycle_start_date_previous   = hindcast_periods[icycle-1]
+            else: 
+                cycle_start_date_previous   = pd.Timestamp("1900-1-1")  # I set a nonsense date (should not be used)
+                     
             cycle_start_date_str    = cycle_start_date.strftime("%Y%m%d")
+            cycle_start_date_previous_str   = cycle_start_date_previous.strftime("%Y%m%d")
             cycle_start_year_str    = cycle_start_date.strftime("%Y")
             cycle_start_month_str   = cycle_start_date.strftime("%m").zfill(2)
             cycle_last_date         = hindcast_periods[icycle+1]-pd.Timedelta(days=1)
@@ -98,27 +108,31 @@ for member in membersList:
             
             #print(n_model_steps)
            
-            cycle_family = hindcast_family.add_family(f"cycle_{icycle}")
+            cycle_family = hindcast_family.add_family(f"CYCLE_{str(icycle).zfill(2)}")
             cycle_family.add_variable("NN_IT000",nit000)
             cycle_family.add_variable("NN_ITEND",nitend)
 
             nit000 += n_model_steps
 
             if icycle > 0:
-                cycle_family.add( Trigger ( f"cycle_{icyclem1} == complete"  ) )
+                cycle_family.add( Trigger ( f"CYCLE_{icyclem1} == complete"  ) )
 
-            cycle_family.add_variable("CYCLE_NUMBER"    , str(icycle).zfill(2))     # 00,01,02          
-            cycle_family.add_variable("CYCLE_START_DATE", cycle_start_date_str)     # YYYYmmdd
-            cycle_family.add_variable("CYCLE_START_YEAR", cycle_start_year_str)     # YYYY
-            cycle_family.add_variable("CYCLE_START_MONTH", cycle_start_month_str)   # mm           
-            cycle_family.add_variable("MODEL_STEPS"     , n_model_steps)            # int
-            cycle_family.add_variable("CYCLE_LAST_DATE" , cycle_last_date_str)     # YYYYmmdd
-            cycle_family.add_variable("CYCLE_DAYS"      , n_days)
+            cycle_family.add_variable("CYCLE_NUMBER"            , str(icycle).zfill(2))             # 00,01,02          
+            cycle_family.add_variable("CYCLE_START_DATE"        , cycle_start_date_str)             # YYYYmmdd
+            cycle_family.add_variable("CYCLE_START_DATE_PREVIOUS", cycle_start_date_previous_str)   # YYYYmmdd
+            cycle_family.add_variable("CYCLE_START_YEAR"        , cycle_start_year_str)             # YYYY
+            cycle_family.add_variable("CYCLE_START_MONTH"       , cycle_start_month_str)            # mm           
+            cycle_family.add_variable("MODEL_STEPS"             , n_model_steps)                    # int
+            cycle_family.add_variable("CYCLE_LAST_DATE"         , cycle_last_date_str)              # YYYYmmdd
+            cycle_family.add_variable("CYCLE_DAYS"              , n_days)                           # int
             
             #if icycle == 0:
             #    task_recup_ic = cycle_family.add_task(f"recup_ic")
 
             task_prepare_run = cycle_family.add_task(f"prepare_run")    
+
+            task_recup_rst = cycle_family.add_task(f"recup_rst")
+            task_recup_rst.add( Trigger( "prepare_run == complete") )            
 
             #task_recup_atm = cycle_family.add_task(f"recup_atm")
             #task_recup_atm.add( Trigger( "prepare_run == complete") )
